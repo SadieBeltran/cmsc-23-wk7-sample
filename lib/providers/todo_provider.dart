9298464c1eaceb6fbd06329f604b4b5f1f4a4639
SadieBeltran <etbeltran1@up.edu.ph>
2022-11-09 14:59:24 +0800

    first commit

---
+++ lib/providers/todo_provider.dart
@@ -0,0 +1,68 @@
+/*
+  Created by: Claizel Coubeili Cepe
+  Date: 27 October 2022
+  Description: Sample todo app with networking
+*/
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:week7_networking_discussion/models/todo_model.dart';
+import 'package:week7_networking_discussion/api/firebase_todo_api.dart';
+
+import '../api/todo_api.dart';
+
+class TodoListProvider with ChangeNotifier {
+  late TodoAPI todoAPI;
+  late Future<List<Todo>> _todoList;
+  late FirebaseTodoAPI firebaseService;
+  late Stream<QuerySnapshot> _todosStream;
+  Todo? _selectedTodo;
+
+  Todo get selected => _selectedTodo!;
+
+  changeSelectedTodo(Todo item) {
+    _selectedTodo = item;
+  }
+
+  Future<List<Todo>> get todo => _todoList;
+
+  TodoListProvider() {
+    firebaseService = FirebaseTodoAPI();
+    fetchTodos();
+  }
+
+  Stream<QuerySnapshot> get todos => _todosStream;
+
+  fetchTodos() {
+    _todosStream = firebaseService.getAllTodos();
+    notifyListeners();
+  }
+
+  void addTodo(Todo item) async {
+    String message = await firebaseService.addTodo(item.toJson(item));
+    print(message);
+    notifyListeners();
+  }
+
+  void editTodo(String newTitle) async {
+    String message =
+        await firebaseService.editTodo(_selectedTodo!.id, newTitle);
+    print(message);
+    // _todoList[index].title = newTitle;
+    notifyListeners();
+  }
+
+  void deleteTodo() async {
+    String message = await firebaseService.deleteTodo(_selectedTodo!.id);
+    print(message);
+    notifyListeners();
+  }
+
+  void toggleStatus(bool status) async {
+    String message =
+        await firebaseService.toggleStatus(_selectedTodo!.id, status);
+    print(message);
+    // _todoList[index].completed = status;
+    notifyListeners();
+  }
+}
